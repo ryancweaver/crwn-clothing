4835d01773e8ec5f95a6645f749a3b60ad3643d7
Weaver, Ryan (US - Mechanicsburg Delivery) <rweaver@deloitte.com>
2021-05-13 14:35:29 -0400

    adding routing to the other pages, and adding local images to project

--- src/App.js
+++ src/App.js
@@ -8,12 +8,40 @@ const HatsPage = () => (
   </div>
 )
 
+const JacketsPage = () => (
+  <div>
+    <h1>Jackets Page</h1>
+  </div>
+)
+
+const SneakersPage = () => (
+  <div>
+    <h1>Sneakers Page</h1>
+  </div>
+)
+
+const MensPage = () => (
+  <div>
+    <h1>Mens Page</h1>
+  </div>
+)
+
+const WomensPage = () => (
+  <div>
+    <h1>Womens Page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/jackets' component={JacketsPage} />
+        <Route path='/shop/sneakers' component={SneakersPage} />
+        <Route path='/shop/mens' component={MensPage} />
+        <Route path='/shop/womens' component={WomensPage} />
       </Switch>
       
     </div>
